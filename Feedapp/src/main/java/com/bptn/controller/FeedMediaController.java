320b4b3e9ff61348c011070ff1ee8b6a03ef9549
Hrithik Kumar Advani <hrithik.kumaradvani@mail.utoronto.ca>
2022-11-10 13:57:45 -0500

    Added image/metadata endpoint in FeedMediaController

--- Feedapp/src/main/java/com/bptn/controller/FeedMediaController.java
+++ Feedapp/src/main/java/com/bptn/controller/FeedMediaController.java
@@ -3,15 +3,19 @@ package com.bptn.controller;
 
 import com.bptn.exceptions.InvalidImageMetaDataException;
 import com.bptn.models.ImageMetaData;
+import com.bptn.request.FeedMediaRequest;
 import com.bptn.service.FeedMediaService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -54,6 +58,23 @@ public class FeedMediaController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
 
+        return new ResponseEntity<>(imageMetaData, HttpStatus.OK);
+    }
+
+    /*
+    Sample Request Payload:
+    {
+    "imageName": "Pizza",
+    "imageSize": "648",
+    "imageFormat": "png",
+    "resolution": "4K",
+    "postkey": "11073"
+     }
+     */
+    @PostMapping(value = "image/metadata", consumes = MediaType.APPLICATION_JSON_VALUE, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Object> createNewImage(@RequestBody FeedMediaRequest feedMediaRequest){
+        LOGGER.debug("Executing createNewImage API");
+        ImageMetaData imageMetaData = feedMediaService.createNewImage(feedMediaRequest);
         return new ResponseEntity<>(imageMetaData, HttpStatus.OK);
     }
 
