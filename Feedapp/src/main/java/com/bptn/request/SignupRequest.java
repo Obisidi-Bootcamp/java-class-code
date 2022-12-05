76a093f018fa853b7f1af09b692c81d18f59f152
Hrithik Kumar Advani <hrithik.kumaradvani@mail.utoronto.ca>
2022-12-05 14:04:21 -0500

    Added Signup Request

---
+++ Feedapp/src/main/java/com/bptn/request/SignupRequest.java
@@ -0,0 +1,61 @@
+package com.bptn.request;
+
+public class SignupRequest {
+
+    private String username;
+    private String name;
+    private String emailId;
+    private String phoneNumber;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "SignupRequest{" +
+                "username='" + username + '\'' +
+                ", name='" + name + '\'' +
+                ", emailId='" + emailId + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
